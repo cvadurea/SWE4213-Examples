914b42b81a43a2bbe4d00802e5dfd5eea56979da
eeddy <eeddy@unb.ca>
2026-01-07 06:48:02 -0400

    Updated example 1

--- Unit1/VSCode/Example1/main.py
+++ Unit1/VSCode/Example1/main.py
@@ -1,3 +1,10 @@
 
+"""
+To run these files you can install the Code Runner extension.
+
+Learning Outcome:
+- Show that I can easily run different types of files. 
+- Show that I can change the runtime of my python file.
+"""
 if __name__ == "__main__":
     print("Hello, World!")
